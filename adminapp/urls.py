5a7332e5bd1d37e9685b28463c233acb76bd6927
phanee27 <venkataphaneendra8@gmail.com>
2025-03-23 17:52:59 +0530

    Added paginator

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('studentdetails/',views.studentdetails,name='studentdetails'),
-    path('get_student_info/<str:student_id>/', views.get_student_info, name='get_student_info'),
-    path("upload/", views.upload_csv, name="upload_csv"),
+    # path('get_student_info/<str:student_id>/', views.get_student_info, name='get_student_info'),
+    # path("upload/", views.upload_csv, name="upload_csv"),
 ]
